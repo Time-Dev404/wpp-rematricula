e18d7d5825da07fc4c001d35aeba70acdc4cf18d
Renan <r.andrade@acad.ifma.edu.br>
2021-04-18 21:25:33 -0300

    Refactor data #6

--- src/messages/msg_save_student_name.js
+++ src/messages/msg_save_student_name.js
@@ -21,7 +21,7 @@ export default async (client, message) => {
     
     await connection('students').where({cpf: cpf}).update({name: message.body});
     await saveInCache(message.from, "set_registration_student");
-    return `Que nome lindo, ${message.body.split(" ")[0]}! Agora me informe sua matrícula`;
+    return `Que nome lindo, ${message.body.split(" ")[0]}! Agora me informe sua matrícula.`;
   } catch (error) {
     console.log("erro :( ", error);
     return `Que pena. Parece que houve um erro. Tenta continuar mais tarde, tá bom?!`;
